1616561ea400215560f98576c2d99348acf1d3ed
goncalvesFe <goncalves.fe1992@gmail.com>
2021-01-26 18:54:54 -0300

    feat: create user class and mocked users

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2013/07/13/12/06/woman-159169_960_720.png'),
+  '4': const User(
+      id: '4',
+      name: 'Guilherme',
+      email: 'guigui@abcmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png')
+};
