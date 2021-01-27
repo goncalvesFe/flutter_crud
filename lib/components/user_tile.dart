c78643ddf0600e8e01617e972d890494659f3244
goncalvesFe <goncalves.fe1992@gmail.com>
2021-01-27 20:21:59 -0300

    feat: add routes and user form page

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
 
 class UserTile extends StatelessWidget {
   final User user;
@@ -23,7 +24,12 @@ class UserTile extends StatelessWidget {
               IconButton(
                 icon: Icon(Icons.edit),
                 color: Colors.orange,
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.of(context).pushNamed(
+                    AppRoutes.USER_FORM,
+                    arguments: user,
+                  );
+                },
               ),
               IconButton(
                 icon: Icon(Icons.delete),
