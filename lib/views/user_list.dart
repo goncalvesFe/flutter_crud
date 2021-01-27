6e0d1ab5bf7cd67556fa08d6a76547cfd39e3acc
goncalvesFe <goncalves.fe1992@gmail.com>
2021-01-27 20:08:49 -0300

    feat: Metodos de adicionar ou atualizar e remover usuario

--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/components/user_tile.dart';
+import 'package:flutter_crud/models/user.dart';
 import 'package:flutter_crud/provider/users.dart';
 import 'package:provider/provider.dart';
 
@@ -14,7 +15,14 @@ class UserList extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              users.put(User(
+                id: '2',
+                name: 'Rafa',
+                email: 'Rafa@aksjdhaskd',
+                avatarUrl: '',
+              ));
+            },
           )
         ],
       ),
