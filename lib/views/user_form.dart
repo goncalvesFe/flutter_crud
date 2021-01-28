fcc4244454d94ca4547c599500758a72c770851b
goncalvesFe <goncalves.fe1992@gmail.com>
2021-01-28 20:24:20 -0300

    feat: add alter profile data function

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -7,8 +7,21 @@ class UserForm extends StatelessWidget {
   final _form = GlobalKey<FormState>();
   final Map<String, String> _formData = {};
 
+  void _loadFormData(User user) {
+    if (user != null) {
+      _formData["id"] = user.id;
+      _formData["name"] = user.name;
+      _formData["email"] = user.email;
+      _formData["avatarUrl"] = user.avatarUrl;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final User user = ModalRoute.of(context).settings.arguments;
+
+    _loadFormData(user);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário de usuário'),
@@ -41,6 +54,7 @@ class UserForm extends StatelessWidget {
           child: Column(
             children: <Widget>[
               TextFormField(
+                initialValue: _formData['name'],
                 decoration: InputDecoration(labelText: 'Nome'),
                 validator: (value) {
                   if (value == null || value.trim().isEmpty) {
@@ -54,9 +68,11 @@ class UserForm extends StatelessWidget {
                 onSaved: (value) => _formData['name'] = value,
               ),
               TextFormField(
+                  initialValue: _formData['email'],
                   decoration: InputDecoration(labelText: 'E-mail'),
                   onSaved: (value) => _formData['email'] = value),
               TextFormField(
+                  initialValue: _formData['avatarUrl'],
                   decoration: InputDecoration(labelText: 'URL do Avatar'),
                   onSaved: (value) => _formData['avatarUrl'] = value)
             ],
